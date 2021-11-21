cace97a2b59de5b41cacc578adae80f7c33f7c45
Lasith Manujitha <64279853+z1nc0r3@users.noreply.github.com>
2021-11-21 22:26:30 +0530

    Update zLyricsFinder.py

--- zLyricsFinder.py
+++ zLyricsFinder.py
@@ -3,8 +3,8 @@ import pyperclip as pc
 
 genius = lg.Genius("CmK8zyrlpjTMptRr9Jh2HVnOsUh7Bj8SCNmkvKlzWSk8P7_L4ZdCtalbR_cyxzQ8", skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
 
-song = input("Enter the song : ")
-artist = input("Enter the artist name : ")
+song = input("Name of the Song : ")
+artist = input("Name of the Artist : ")
 
 try: 
     pc.copy(genius.search_song(song, artist).lyrics)
